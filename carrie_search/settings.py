714950c704e183ca8d5c254db62c3f64dd3a8397
Vikas Sanwal <vikas.sanwal1@oodles.io>
2025-06-24 02:00:40 +0530

    Isolate send email config for dev and prod

--- carrie_search/settings.py
+++ carrie_search/settings.py
@@ -18,16 +18,24 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+ENV = os.getenv("ENV")
+
 # loading the perplexity api
 PERPLEXITY_API_KEY = os.getenv("PERPLEXITY_API_KEY")
 
 # Email settings for Elastic Email
-EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+
+if ENV == "dev":
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = 587
+    EMAIL_HOST_USER = DEFAULT_FROM_EMAIL
+    EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+    EMAIL_USE_TLS = True
+else:
+    EMAIL_BACKEND = "sgbackend.SendGridBackend"
+    SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
 
 
 # STRIPE CONFIG
